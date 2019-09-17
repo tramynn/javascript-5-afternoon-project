c30dea08de4d7e4a4efc2f98b72a00ccbdd98fdc
Tramy Nguyen <tramynn2@gmail.com>
2019-09-17 14:23:43 -0500

    completed closures

--- closures.js
+++ closures.js
@@ -10,34 +10,32 @@
 
 // Do not edit the code below.
 function outer() {
-  var name = 'Tyler';
+  var name = "Tyler";
   return function() {
-    return 'The original name was ' + name;
+    return "The original name was " + name;
   };
 }
 // Do not edit the code above.
-  
+
 /* 
   Above you're given a function that returns another function which has a closure over the name variable.
   Invoke outer saving the return value into another variable called 'inner'.
 */
-  
+
 // Code Here
-
-
+const inner = outer();
 
 //Once you do that, invoke inner.
 
 //Code Here
-
-
+inner();
 
 ////////// PROBLEM 2 //////////
 
 // Do not edit the code below.
 function callFriend(name) {
   function dial(number) {
-    return 'Calling ' + name + ' at ' + number
+    return "Calling " + name + " at " + number;
   }
   return dial;
 }
@@ -52,8 +50,8 @@ function callFriend(name) {
 */
 
 //Code Here
-
-
+const callJake = callFriend("Jake");
+callJake("435-555-9248");
 
 ////////// PROBLEM 3 //////////
 
@@ -62,17 +60,20 @@ function callFriend(name) {
 */
 
 //Code Here
-
-
+function makeCounter() {
+  let num = 0;
+  return function increment() {
+    num++;
+    return num;
+  };
+}
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
-
-
+var count = makeCounter();
+count(); // 1
+count(); // 2
+count(); // 3
+count(); // 4
 
 ////////// PROBLEM 4 //////////
 
@@ -87,19 +88,26 @@ function callFriend(name) {
 
 function counterFactory(value) {
   // Code here.
-
+  // no need to initialize value bc you're directly affecting the passed argument
+  function inc() {
+    value++;
+    return value;
+  }
+  function dec() {
+    value--;
+    return value;
+  }
   return {
-
+    inc,
+    dec
   };
 }
 
 counter = counterFactory(10);
-// counter.inc() // 11
-// counter.inc() // 12
-// counter.inc() // 13
-// counter.dec() // 12
-
-
+counter.inc(); // 11
+counter.inc(); // 12
+counter.inc(); // 13
+counter.dec(); // 12
 
 ////////// PROBLEM 5 //////////
 
@@ -109,18 +117,19 @@ counter = counterFactory(10);
   (Hint: don't forget to have a space between the firstname and lastname and a period at the end of the sentence.)
 */
 
-function motivation( firstname, lastname ) {
+function motivation(firstname, lastname) {
   var welcomeText = "You're doing awesome, keep it up";
 
   // code message function here.
+  function message() {
+    return welcomeText + " " + firstname + " " + lastname + ".";
+  }
 
-  //Uncommment this to return the value of your message function
-  //return message;
+  // Uncommment this to return the value of your message function
+  return message;
 }
 
-var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
-
-
+var greeting = motivation("Billy", "Bob"); // 'You're doing awesome keep it up Billy Bob.
 
 ////////// PROBLEM 6 //////////
 
@@ -136,18 +145,27 @@ var module = (function() {
     location: "Utah"
   };
 
-  function privateMethod(){
-    return "Hi, I'm " + person.name + ", age " + person.age + " from " + person.location;
+  function privateMethod() {
+    return (
+      "Hi, I'm " +
+      person.name +
+      ", age " +
+      person.age +
+      " from " +
+      person.location
+    );
   }
 
   // Anything that is being returned is made public and can be invoked from
   // outside our lexical scope
   return {
     // Code here.
+    publicMethod: function() {
+      // don't forget to invoke methods
+      return privateMethod();
+    }
   };
 })();
-
-
 
 ////////// PROBLEM 7 //////////
 
@@ -163,13 +181,19 @@ function secretNumber() {
 
   return {
     // Code here
+    addToSecret: function(num) {
+      secret += num;
+      return secret;
+    },
+    takeAwayFromSecret: function(num) {
+      secret -= num;
+      return secret;
+    }
   };
 }
 
-
-
 ////////// PROBLEM 8 //////////
-  
+
 /*
   Here we have a for loop that will iterate as long as i is less than or equal to 5.
   What we need to do is console.log(i) so that it logs like so:
